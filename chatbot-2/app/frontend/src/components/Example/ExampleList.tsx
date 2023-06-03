669420780609be3faedc825cfe814633296ae85a
Vishakh Arora <vishakh.arora29@gmail.com>
2023-06-03 00:22:25 +0000

    Updated bot to train on VC textbook, changed example questions in frontend

---
+++ chatbot-2/app/frontend/src/components/Example/ExampleList.tsx
@@ -0,0 +1,33 @@
+import { Example } from "./Example";
+
+import styles from "./Example.module.css";
+
+export type ExampleModel = {
+    text: string;
+    value: string;
+};
+
+const EXAMPLES: ExampleModel[] = [
+    {
+        text: "What is pre money valuation?",
+        value: "What is pre money valuation?"
+    },
+    { text: "What is a term sheet?", value: "What is a term sheet?" },
+    { text: "What is a liquidation preference?", value: "What is a liquidation preference?" }
+];
+
+interface Props {
+    onExampleClicked: (value: string) => void;
+}
+
+export const ExampleList = ({ onExampleClicked }: Props) => {
+    return (
+        <ul className={styles.examplesNavList}>
+            {EXAMPLES.map((x, i) => (
+                <li key={i}>
+                    <Example text={x.text} value={x.value} onClick={onExampleClicked} />
+                </li>
+            ))}
+        </ul>
+    );
+};
